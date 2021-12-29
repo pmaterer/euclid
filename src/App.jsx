669a06a319b70e3337e45ba29b3df99b364178d7
Patrick Materer <patrickmaterer@gmail.com>
2021-12-29 11:38:32 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-red-400">React Template</h1>
+      <p>
+        This place is not a place of honor... no highly esteemed deed is
+        commemorated here... nothing valued is here.
+      </p>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: process.env.NODE_ENV === "prod" ? "production" : "development",
+  entry: path.resolve(__dirname, "./src/index.js"),
+  target: "web",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          "postcss-loader",
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/templates/index.html"),
+      title: "React Template",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+  devServer: {
+    port: "auto",
+    compress: true,
+    hot: true,
+    open: true,
+  },
+};
